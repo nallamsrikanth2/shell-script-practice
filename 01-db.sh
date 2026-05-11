630d303955495c3a823ca1d01fb23bc23d8d2736
nallamsrikanth12 <nallamsrikanth2@gmail.com>
2026-05-11 14:42:40 +0530

    shell

---
+++ 01-db.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R please run the root user $N"
+    exit 1
+else
+    echo -e "$G you are a root user $N"
+fi
+
+
+VALIDATE(){
+
+}
+
